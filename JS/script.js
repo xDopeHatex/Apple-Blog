45643881e14a243c9925df324613826ad0357418
xDopeHatex <xgorokhovx@gmail.com>
2022-10-15 16:04:52 +0400

    addform

--- JS/script.js
+++ JS/script.js
@@ -17,10 +17,8 @@ const addPost = document.querySelector(".add-post");
 const overlay = document.querySelector(".overlay");
 const postContent = document.querySelector("#post-content");
 const titleContent = document.querySelector("#title-content");
-const form = document.querySelector("#form");
+const addForm = document.querySelector("#add-form");
 const postList = document.querySelector(".post-list");
-const btnDelete = document.querySelector(".delete-button");
-const btnEdit = document.querySelector(".edit-button");
 
 uploadPosts();
 
@@ -44,7 +42,7 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-form.addEventListener("submit", addPostToServer);
+addForm.addEventListener("submit", addPostToServer);
 
 async function addPostToServer(e) {
   if (postContent.value === "") {
@@ -150,14 +148,33 @@ async function uploadPosts() {
   result.items.forEach((element) => {
     template(element);
   });
+
+  const btnDelete = document
+    .querySelectorAll(".delete-button")
+    .forEach((el) => el.addEventListener("click", deletePost));
+
+  const btnEdit = document
+    .querySelectorAll(".edit-button")
+    .forEach((el) => el.addEventListener("click", editPost));
+
+  const btnLike = document
+    .querySelectorAll(".like-button")
+    .forEach((el) => el.addEventListener("click", likePost));
+
+  // btnDelete.addEventListener("click", deletePost);
+  // btnEdit.addEventListener("click", editPost);
+
+  window.addEventListener("DOMContentLoaded", (event) => {
+    [...document.querySelectorAll("a[href^='example.com']")].forEach((el) =>
+      el.addEventListener("click", function (e) {
+        newrelic.addPageAction("Doc");
+      })
+    );
+  });
 }
 
 function template(item) {
   const data = new Date(item.created);
-
-  function del(item) {
-    return () => deletePost(item);
-  }
 
   const currentTime = data.toLocaleDateString("ru", {
     day: "2-digit",
@@ -167,21 +184,21 @@ function template(item) {
 
   const li = document.createElement("li");
   // Add class
-  li.className = `flex flex-col justify-between p-6 bg-slate-100 space-y-8 rounded-lg`;
+  li.className = `post flex flex-col justify-between p-6 bg-slate-100 space-y-8 rounded-lg`;
 
   //Add id
 
   li.setAttribute("id", `${item.id}`);
 
-  li.innerHTML = `<div class="flex justify-between items-center">
-            <h2 class="text-2xl">${item.title}</h2>
-            <div class="flex items-center space-x-3">
-              <button
+  li.innerHTML = `<div class="flex justify-between items-center ">
+            <h2 class="title text-2xl">${item.title}</h2>
+            <div class="flex items-center  space-x-3" >
+              <button 
                 class="edit-button px-7 py-2 bg-blue-400 rounded-2xl text-white hover:bg-blue-300 active:bg-blue-700 hover:shadow-lg hover:shadow-slate-500 transition-all duration-200 active:translate-y-0.5"
               >
                 Edit
               </button>
-              <button onclick="del()"
+              <button 
                 class="delete-button px-4 py-2 bg-blue-400 rounded-2xl text-white hover:bg-blue-300 active:bg-blue-700 hover:shadow-lg hover:shadow-slate-500 transition-all duration-200 active:translate-y-0.5"
               >
                 Delete
@@ -189,83 +206,98 @@ function template(item) {
             </div>
           </div>
           
-          <div>
+          <div class='body'>
            ${item.body}
           </div>
           
           <div class="flex justify-between items-center div-likes">
-            <button  class="relative " onclick=" ${() => g()}" >
-              <p class="ml-8">${item.likes}</p>
+            <button  class="relative like-button" >
+              <p class="likes ml-8">${item.likes}</p>
               <div
                 class="h-6 w-6 absolute top-0.5 -left-0 hover:scale-125 rounded-2xl hover:bg-pink-500 transition-all active:translate-y-1 hover:shadow-lg hover:shadow-slate-500 duration-300"
               >
-                <img class="button-like"  id="${
-                  item.id
-                }" src="../img/like.svg" style="fill: white" alt="" />
+                <img    src="../img/like.svg" style="fill: white" alt="" />
               </div>
             </button>
             <div>${currentTime}</div>
           </div>`;
 
-  // const theFirstChild = postList.firstChild;
-
-  // postList.insertBefore(li, theFirstChild);
-
   postList.appendChild(li);
 }
 
-async function editLikeNumber(object) {
-  console.log(object);
+async function likePost(e) {
+  const parentDiv = e.target.closest(".post");
+  const likes = parentDiv.querySelector(".likes").textContent;
+  const body = parentDiv.querySelector(".body").textContent;
+  const title = parentDiv.querySelector(".title").textContent;
+
+  let totalLikes = Number(likes) + 1;
+
+  console.log(body);
+
   await fetch(
-    `https://pocketbase.sksoldev.com/api/collections/blog/records/${object.id}`,
+    `https://pocketbase.sksoldev.com/api/collections/blog/records/${parentDiv.id}`,
     {
       method: "PATCH",
       body: JSON.stringify({
-        likes: object.likes++,
-        body: object.body,
-        title: object.title,
+        likes: totalLikes,
+        body: body,
+        title: title,
+      }),
+
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  );
+  postList.innerHTML = "";
+  uploadPosts();
+}
+
+// editForm.addEventListener("submit", editPost);
+
+async function editPost(e) {
+  const parentDiv = e.target.closest(".post");
+  const likes = parentDiv.querySelector(".likes").textContent;
+  const body = parentDiv.querySelector(".body").textContent;
+  const title = parentDiv.querySelector(".title").textContent;
+
+  const response = await fetch(
+    `https://pocketbase.sksoldev.com/api/collections/blog/records/${parentDiv.id}`,
+    {
+      method: "PATCH",
+      body: JSON.stringify({
+        likes: likes,
+        body: body,
+        title: title,
       }),
       headers: {
         "Content-type": "application/json; charset=UTF-8",
       },
     }
   );
+  const post = await response.json();
 
-  console.log("------");
+  console.log(post);
+
+  currentTask.body = post.body;
+
+  let li = document.getElementById(`${id}`);
+
+  li.innerHTML = "";
+
+  li.innerHTML = ``;
 }
 
-// async function editLikeNumber(e) {
-//   const response = await fetch(
-//     `https://pocketbase.sksoldev.com/api/collections/blog/records/${id}`,
-//     {
-//       method: "PATCH",
-//       body: JSON.stringify({
-//         body: taskInputCurrent.value,
-//       }),
-//       headers: {
-//         "Content-type": "application/json; charset=UTF-8",
-//       },
-//     }
-//   );
-//   const post = await response.json();
+async function deletePost(e) {
+  const parentDiv = e.target.closest(".post");
 
-//   console.log(post);
-
-//   currentTask.body = post.body;
-
-//   let li = document.getElementById(`${id}`);
-
-//   li.innerHTML = "";
-
-//   li.innerHTML = ``;
-// }
-
-async function deletePost(id) {
-  console.log("______");
   await fetch(
-    `https://pocketbase.sksoldev.com/api/collections/blog/records/${id}`,
+    `https://pocketbase.sksoldev.com/api/collections/blog/records/${parentDiv.id}`,
     {
       method: "DELETE",
     }
   );
+  postList.innerHTML = "";
+  uploadPosts();
 }
